a13a8a22d601d4d7dfd305dec3393f75511bf00e
Michał Fal <michfal86@gmail.com>
2020-08-05 16:38:44 +0200

    logic base

--- js/accountLogic.js
+++ js/accountLogic.js
@@ -2,17 +2,28 @@ class AccountLogic{
     constructor(){
         this.setBet = document.querySelector('.bet');
         this.currentBet = [];
+        this.betIndex = 0;
     }
 
     bet(e, basicBets){
         const target = e.target;
         if(target.classList.contains('bet_btn')){
-            this.currentBet = basicBets[0]
-            console.log(this.currentBet)
-            if(target.classList.contains('bet_one')) console.log(this.currentBet)
-            if(target.classList.contains('bet_max')) console.log("bet max")
+            // this.currentBet = basicBets[0]
+            // console.log(this.currentBet)
+            if(target.classList.contains('bet_one')) {
+                if(this.betIndex < basicBets.length)this.betIndex++;
+                console.log(basicBets[this.betIndex])
+            }
+            if(target.classList.contains('bet_max')) {
+                this.betIndex = basicBets.length - 1;
+                console.log(basicBets[this.betIndex])
+            }
         }
-        
+    }
+    displayBet(e, basicBets) {
+        if(target.classList.contains('cash_value')) {
+            target.innerHtml = basicBets[this.betIndex]
+        }
     }
 
 }
